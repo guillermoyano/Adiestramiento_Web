7c203c278fa26f2afc68e7e9d0e632bcaf6d1a07
guillermoyano <guillermoyano2002@gmail.com>
2023-05-24 21:47:44 -0300

    ultimo commit 24-5

--- src/main/java/KaveCanem/adiestramientoweb/servicios/RutinaServicio.java
+++ src/main/java/KaveCanem/adiestramientoweb/servicios/RutinaServicio.java
@@ -1,9 +1,13 @@
 package KaveCanem.adiestramientoweb.servicios;
 
 import KaveCanem.adiestramientoweb.entidad.Rutina;
+import KaveCanem.adiestramientoweb.entidad.Tutor;
 import KaveCanem.adiestramientoweb.repositorios.RutinaRepositorio;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -15,7 +19,7 @@ public class RutinaServicio {
     @Autowired
     private RutinaRepositorio rutinaRepositorio;
     
-    
+    @Transactional
     public void crearRutina(String paseo, String frecPaseo, String herramientas, String salida, String observacionesPaseo,
             String calle, String comida, String frecComida, String observacionesComida, String juego, String juegaCon,
             String dispoJuguetes, String frecJuego, String observacionesJuego, String duerme, String frecDuerme,
@@ -48,6 +52,21 @@ public class RutinaServicio {
         
     }
 
+    
+     public List<Rutina> listarRutina(){
+        
+        List<Rutina> rutinas = new ArrayList();
+        
+        rutinas = rutinaRepositorio.findAll();
+        
+        return rutinas;
+        
+    }
+     
+     public void modificarRutina(){
+         
+     }
+    
 }
 
 
--- src/main/java/KaveCanem/adiestramientoweb/servicios/TutorServicio.java
+++ src/main/java/KaveCanem/adiestramientoweb/servicios/TutorServicio.java
@@ -4,9 +4,13 @@ import KaveCanem.adiestramientoweb.entidad.Perro;
 import KaveCanem.adiestramientoweb.entidad.Tutor;
 import KaveCanem.adiestramientoweb.repositorios.PerroRepositorio;
 import KaveCanem.adiestramientoweb.repositorios.TutorRepositorio;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -14,30 +18,66 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class TutorServicio {
-    
+
     @Autowired
     private TutorRepositorio tutorRepositorio;
     @Autowired
     private PerroRepositorio perroRepositorio;
-    
-    public void crearTutor(String nombre, String apellido, Long telefono, String direccion, Date fechaAlta, String idPerro){
-        
+
+    @Transactional
+    public void crearTutor(String nombre, String apellido, Long telefono, String direccion, Date fechaAlta, String idPerro) {
+
         Perro perro = perroRepositorio.findById(idPerro).get();
         Tutor tutor = new Tutor();
-        
+
         tutor.setNombre(nombre);
         tutor.setApellido(apellido);
         tutor.setTelefono(telefono);
         tutor.setDireccion(direccion);
         tutor.setFechaAlta(fechaAlta);
         tutor.setPerro(perro);
-        
+
         tutorRepositorio.save(tutor);
-        
-        
+
+    }
+
+    public List<Tutor> listarTutores() {
+
+        List<Tutor> tutores = new ArrayList();
+
+        tutores = tutorRepositorio.findAll();
+
+        return tutores;
+
+    }
+
+    public void modificarTutor(String idTutor, String nombre, String apellido, Long telefono, String direccion, String idPerro) {
+
+        Optional<Tutor> respuesta = tutorRepositorio.findById(idTutor);
+        Optional<Perro> respuestaPerro = perroRepositorio.findById(idPerro);
+
+        Perro perro = new Perro();
+
+        if (respuestaPerro.isPresent()) {
+
+            perro = respuestaPerro.get();
+
+        }
+
+        if (respuesta.isPresent()) {
+
+            Tutor tutor = respuesta.get();
+
+            tutor.setNombre(nombre);
+            tutor.setApellido(apellido);
+            tutor.setTelefono(telefono);
+            tutor.setDireccion(direccion);
+            tutor.setPerro(perro);
+            
+            tutorRepositorio.save(tutor);
+
+        }
+
     }
 
 }
-
-
- 
